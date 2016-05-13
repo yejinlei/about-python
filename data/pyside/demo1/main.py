421a22a6ca1b53dbcd059c452d57a461bc74b3d7
4010896@qq.com <4010896@qq.com>
2016-05-13 21:31:19 +0800

    图形界面pyside：顶级窗体

--- data/pyside/demo1/main.py
+++ data/pyside/demo1/main.py
@@ -1,8 +1,14 @@
+#coding:UTF-8
+
 import sys
 from PySide import QtGui
 
 app = QtGui.QApplication(sys.argv)
 app.aboutToQuit.connect(app.deleteLater)
-hello = QtGui.QPushButton("Hello world!")
+hello = QtGui.QPushButton("Hello world!") #顶级窗体
+hello.setGeometry(300, 300, 250, 150)    #窗体位置和大小
+hello.setToolTip(u'我是顶级窗口')      #气泡提示
+QtGui.QPushButton("yee", hello)         #控件
+hello.setWindowTitle(u'Demo')          #标题
 hello.show()
 app.exec_()
