36c178526887cb05494ce2ad79b1e7cd69d53bcb
4010896@qq.com <4010896@qq.com>
2016-06-05 22:53:25 +0800

    决策树：新增选择函数

--- data/pyml/dicisiontree.py
+++ data/pyml/dicisiontree.py
@@ -4,14 +4,27 @@ from __future__ import division
 from math import log
 from collections import *
 
-def calcEntropy(dataset):
-    n = len(dataset)
+def createDTree():
+    pass
+
+def selDataset(dataSet, selCol, selValue):
+    newdataSet = []
+    for dataRow in dataSet:
+        if dataRow[selCol] == selValue:
+            head = dataRow[:selCol]
+            head.extend(dataRow[selCol + 1:])
+            newdataSet.append(head)
+    return newdataSet
+
+def calcEntropy(dataSet):
+    n = len(dataSet)
     entropy = 0.0
-    datadict = Counter(dataset)
-    for c in datadict.itervalues():
+    dataDict = Counter(dataSet)
+    for c in dataDict.itervalues():
         p = c / n
         entropy -= p * log(p, 2)
     return entropy
 
 if __name__ == '__main__':
     print calcEntropy([1,1,2,2])
+    print selDataset([[1,2,'yes'],[1,1,'yes'],[0,2,'no']], 0, 1)
