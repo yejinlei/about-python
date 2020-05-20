7abfaf09dea4258761ac356fa856ca2c0bda645b
4010896@qq.com <4010896@qq.com>
2020-05-21 00:07:03 +0800

    anaconda安装及配置脚本

---
+++ 运行和执行环境/anaconda_down.bat
@@ -0,0 +1,1 @@
+axel -n 32 https://repo.anaconda.com/archive/Anaconda3-2020.02-Windows-x86_64.exe
---
+++ 运行和执行环境/anaconda_nbextensions.bat
@@ -0,0 +1,26 @@
+CALL conda config --remove-key channels &
+CALL conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/ &
+CALL conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/ &
+CALL conda config --set show_channel_urls yes &
+CALL conda config --remove channels defaults &
+CALL conda config --set ssl_verify true &
+
+@echo off
+c:
+cd %USERPROFILE%
+if not exist %USERPROFILE%\pip (
+    md %USERPROFILE%\pip
+)
+set file=%USERPROFILE%\pip\pip.ini
+if not exist %file% (
+    echo [global]>%file%
+    echo timeout=300>>%file%
+    echo index-url=http://mirrors.aliyun.com/pypi/simple/>>%file%
+    echo [install]>>%file%
+    echo trusted-host=mirrors.aliyun.com>>%file%
+    echo %file%+"创建成功"
+) else ( echo %file%+"已经存在" )
+pause
+
+conda install -c conda-forge jupyter_contrib_nbextensions &
+jupyter contrib nbextension install --user
